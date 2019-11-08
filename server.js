c234c7c1525ebb725acbfb135eeabb970f3596d8
Anya L Keller <kelleranyal@gmail.com>
2019-11-08 00:35:59 -0500

    added basic scraping

--- server.js
+++ server.js
@@ -1,3 +1,63 @@
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHEadlines";
+var express = require('express');
+var logger = require('morgan');
+var mongoose = require('mongoose');
 
-mongoose.conect(MONGODB_URI);
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require('axios');
+var cheerio = require('cheerio');
+
+// Require all models
+var db = require('./models');
+
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger('dev'));
+// Parse request body as JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static('public'));
+
+// Connect to the Mongo DB
+var MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+mongoose.connect(MONGODB_URI);
+
+// Routes
+
+app.get('/articles', function(req, res) {
+  axios.get('https://www.nytimes.com').then(function(response) {
+    var $ = cheerio.load(response.data);
+    var results = [];
+
+    $('article').each(function(i, element) {
+      var title = $(element)
+        .children()
+        .text();
+      var link = $(element)
+        .find('a')
+        .attr('href');
+
+      // Save these results in an object that we'll push into the results array we defined earlier
+      results.push({
+        title: title,
+        link: link
+      });
+    });
+    console.log(results);
+    res.json(results);
+  });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log('App running on port ' + PORT + '!');
+});
